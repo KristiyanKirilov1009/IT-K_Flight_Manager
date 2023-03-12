ec2bead0942c8b93fd775c0f696568ad5d51205b
Kristiyan Kirilov <k.vladimirov2004@gmail.com>
2023-03-13 00:59:42 +0200

    +company ctrl

---
+++ FlightManager/FlightManager/Controllers/CompanyController.cs
@@ -0,0 +1,37 @@
+﻿using FlightManager.Data.Data;
+using FlightManager.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightManager.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly FlightContext _flightContext;
+
+        public CompanyController(FlightContext flightContext)
+        {
+            _flightContext = flightContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Company> companies = _flightContext.Companies;
+            return View(companies);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Company company)
+        {
+            _flightContext.Companies.Add(company);
+            _flightContext.SaveChangesAsync();
+            return View();
+        }
+    }
+}
