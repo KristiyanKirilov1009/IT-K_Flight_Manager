2b0c1b7f46560d6727c3366c23837d51f368c64b
Beray <bsezgin00@gmail.com>
2023-04-08 00:17:27 +0300

    Small front-end changes

--- FlightManager/FlightManager/FlightManager/wwwroot/js/script.js
+++ FlightManager/FlightManager/FlightManager/wwwroot/js/script.js
@@ -60,3 +60,16 @@ document.querySelectorAll(".table-sortable th").forEach(headerCell => {
 })
 
 
+$(function() {
+	'use strict';
+	
+  $('.form-control').on('input', function() {
+	  var $field = $(this).closest('.form-group');
+	  if (this.value) {
+	    $field.addClass('field--not-empty');
+	  } else {
+	    $field.removeClass('field--not-empty');
+	  }
+	});
+
+});
